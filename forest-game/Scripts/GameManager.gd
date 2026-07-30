ce12ac616124a7ef558cd6481d7a7d337a6de5f6
Joshn't <86410587+Joshnt@users.noreply.github.com>
2026-07-30 12:26:45 +0200

    endscreen

--- forest-game/Scripts/GameManager.gd
+++ forest-game/Scripts/GameManager.gd
@@ -6,11 +6,11 @@ signal added_player_moths
 signal deleted_player_moths
 signal game_ended
 
-
-
-
-
+func _ready() -> void:
+	player_moths = 3
 	
+func _process(delta: float) -> void:
+	print(player_moths)
 func add_player_moths(amount: int, new_moth: bool):
 	player_moths += amount
 	if not new_moth: return
