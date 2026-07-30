578804e09f9efef4f0b8aea3beb790e3e343a48c
Joshn't <86410587+Joshnt@users.noreply.github.com>
2026-07-30 17:40:22 +0200

    soundsystgem in

--- forest-game/Scenes/audio_player.gd
+++ forest-game/Scenes/audio_player.gd
@@ -1,9 +1,17 @@
 extends Node
 @export var sounds: Array[AudioStream]
 
-@export var sound_effects: Array[AudioEffect, String]
+var sound_effects := {
+	"jump_sound": "res://Assets/sounds/gtaSounds-SprungLanden.wav",
+	"move_sound": "res://Assets/sounds/gtaSounds-schritte1.wav",
+	"brief_sound": "res://Assets/sounds/gtaSounds-brief.wav",
+	"click_sound": "res://Assets/sounds/gtaSounds-click.wav",
+	"death_sound": "res://Assets/sounds/gtaSounds-deathSound.wav",
+	"motte_collected": "res://Assets/sounds/gtaSounds-motteVerlieren1.wav",
+	"motte_lost": "res://Assets/sounds/gtaSounds-motteFangen.wav",
+}
 
-
+@onready var sound_player: AudioStreamPlayer2D = $SoundPlayer
 @onready var player = $AudioStreamPlayer
 var rng = RandomNumberGenerator.new()
 var volume = 0.0
@@ -19,3 +27,8 @@ func play_random_sound():
 		player.volume_db = rng.randf_range(-7,0)
 		player.play()    
 		await player.finished
+
+
+func play_sound_effect(sound_effect_name: String):
+	sound_player.strem = sound_effects[sound_effect_name]
+	sound_player.play()
