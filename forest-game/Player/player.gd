7e0299bef9719c0cb7464eddc7440d36ef325b5e
Joshn't <86410587+Joshnt@users.noreply.github.com>
2026-07-31 11:55:59 +0200

    anpassungen spieler + motte

--- forest-game/Player/player.gd
+++ forest-game/Player/player.gd
@@ -1,8 +1,8 @@
 class_name Player extends CharacterBody2D
 
-@export var speed := 500.0
+@export var speed := 2000.0
 @export var gravity := 4000.0
-@export var jump_impulse := 1800.0
+@export var jump_impulse := 5000.0
 @export var air_control := 0.9
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var texturen: Node2D = %texturen
